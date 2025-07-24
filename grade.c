b03a1a749885ca01cb6a2983a8dd59e20654742c
Pavithrapavithraa <eshwaranpavithra@gmail.com>
2025-07-24 15:32:54 +0530

    Add files via upload

---
+++ grade.c
@@ -0,0 +1,13 @@
+#include <Stdio.h>
+int main() {
+    int i,j,rows;
+    printf("Enter number of rows:");
+    Scanf("%d", &rows);
+    for(i=1;i <=rows; i++){
+        for(j=1;j <=i;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
